943b61107a3859f371efedd5b825c4ef27900a4e
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-18 18:48:11 +0300

    Added onImgClickCreateModal function to the 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,6 +4,7 @@ import { galleryItems } from './gallery-items.js';
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClickCreateModal);
 
 var lightbox = new SimpleLightbox('.gallery a', { 
     captionsData: "alt"
@@ -12,16 +13,50 @@ var lightbox = new SimpleLightbox('.gallery a', {
 //rendered items
 function createGalleryItemsMarkup(items) {
     return items.map(({preview, original, description}) => {
-        return `<li>
-        <a class="gallery__item" href=${original}>
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
             <img class="gallery__image"
             src="${preview}"
-            data-source="${original}"
             alt="${description}"
            />
         </a>
-        </li>`
+    </div>`
     }).join('');
+}
+
+function onImgClickCreateModal(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {return;}
+
+    const isItemImage = e.target.classList.contains("gallery__image");
+    if (!isItemImage) {
+        return;
+    }
+
+    const currentImgUrl = e.target.dataset.source;
+
+    const instance = basicLightbox.create(
+        `
+		<img src="${currentImgUrl}" width="1280" height="auto"/>
+        `,
+        {
+            onShow: (instance) => {
+                window.addEventListener("keydown", onEscKeyPress);
+            },
+            onClose: (instance) => {
+                window.removeEventListener("keydown", onEscKeyPress);
+            },
+        }
+    );
+    instance.show();
+
+    function onEscKeyPress(event) {
+        const ESC_KEY_CODE = 'Escape';
+        const isEscKey = event.code === ESC_KEY_CODE;
+        if (isEscKey || onclick) {
+            instance.close();
+        }
+    }
 }
 
 
