2969738196e65759fa7a101f0ec84e7ea7e2d6fb
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-18 16:59:38 +0300

    Lightbox task completed

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt"
+ });
+
+//rendered items
+function createGalleryItemsMarkup(items) {
+    return items.map(({preview, original, description}) => {
+        return `<li>
+        <a class="gallery__item" href=${original}>
+            <img class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+           />
+        </a>
+        </li>`
+    }).join('');
+}
+
+
 console.log(galleryItems);
